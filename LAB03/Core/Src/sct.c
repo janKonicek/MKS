1f073ed4af263ebf22531c174ae50fedb81553e8
janKonicek <jan.konicek610@gmail.com>
2025-10-15 10:24:18 +0200

    LAB03 - typo

--- LAB03/Core/Src/sct.c
+++ LAB03/Core/Src/sct.c
@@ -61,7 +61,7 @@ void sct_init(void) {
 void sct_led(uint32_t value) {
 
 	for (uint32_t i = 0; i < 32; i++) {
-		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, value&1);	// Write value to SDI
+		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, value & 1);	// Write value to SDI
 		value >>= 1;
 		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);	// Pulse on CLK
 		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
