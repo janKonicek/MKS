d41e42f5c2d460d21f5d4ca19426a026339d5dff
janKonicek <jan.konicek610@gmail.com>
2025-10-29 08:54:40 +0100

    LAB06 - DS18B20 Temp

---
+++ LAB06/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: janko
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+void sct_init(void);
+void sct_led(uint32_t value);
+
+void sct_value(uint16_t value, uint8_t led, uint8_t DP);
+
+#endif /* INC_SCT_H_ */
